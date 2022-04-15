a721d23ce481ec5dcf36a3ec043e95fa154337fe
mandaapag03 <paganiamanda791@gmail.com>
2022-04-15 18:34:13 -0300

    Conserta programa de serviços

--- pythonProject/ano_bissexto_py.py
+++ pythonProject/ano_bissexto_py.py
@@ -1,6 +1,6 @@
 #Programa que descobre anos bissextos e não bissextos.
 ano = int(input('Informe um ano: '))
-if ano % 400 == 0 or ano % 4 == 0 and not(ano % 100 == 0):
-    print('O ano é bissexto!!!')
+if 400 != 0 and (4 != 0 or (ano % 100 == 0)):
+    print('O ano não é bissexto ;-;')
 else :
-        print('O ano não é bissexto ;-;')
+        print('O ano é bissexto!!!')
--- pythonProject/contratacao_de_servico.py
+++ pythonProject/contratacao_de_servico.py
@@ -1,10 +1,7 @@
 #Programa que confirma contrato de um serviço:
-
 nome = (input('Por favor informe seu nome: '))
 print('Olá ',nome,', tudo bem?')
 pergunta =int(input('Deseja contratar nosso serviço?(1 - yes/0 - no) : '))
-#cuidado com o loop do while!!!!
-#nao execute o programa
-#while pergunta == 0: print('Deseja contratar nosso serviço?(1 - yes/0 - no) : ')
+while pergunta == 0: pergunta =int(input('TEM CERTEZA QUE NÃO QUER contratar nosso serviço???????(1 - yes/0 - no) : '))
 if pergunta == 1 : print('Obrigado por confiar no nosso serviço {0}! Estamos felizes em ter você conosco'.format(nome))
 else: print('Então ta bom {0} ;-;'.format(nome))
