0dc03903247d24c962d2008dfa1f06059fe9a4ba
mandaapag03 <paganiamanda791@gmail.com>
2022-04-18 14:12:08 -0300

    Tenta adicionar lower() para variável comandos

--- pythonProject/Escape/jogo_de_labirinto.py
+++ pythonProject/Escape/jogo_de_labirinto.py
@@ -1,6 +1,7 @@
-print('Você está em uma floresta escuta, a luz do luar ilimina os itens a sua frente....')
+print('Você está em uma floresta escura, a luz do luar ilumina os itens a sua frente....')
 print('--> Itens: PÁ e LANTERNA')
 comandos = input('Pegar itens??(y - Yes / n - No): ')
+comandos.lower()
 if comandos == 'y':
     print('Você pegou os itens! ')
 elif comandos == 'n':
@@ -11,13 +12,15 @@ elif comandos == 'sair':
         print('Fim de jogo!')
 else :
     print('REINICIE O JOGO')
-
+    
 print('='*100)
 print('Você tem dois caminhos, um ao NORTE e o outro a LESTE...')
 comandos = input('NORTE ou LESTE?')
+comandos.lower()
 if comandos == 'NORTE':
     print('Você foi para o norte')
 elif comandos == 'LESTE':
     print('Você foi para o leste')
+
 
 
