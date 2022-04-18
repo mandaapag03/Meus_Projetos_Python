4a9122b8494dd4ad1eee3632a89e978d33c5ee7e
mandaapag03 <paganiamanda791@gmail.com>
2022-04-17 22:32:16 -0300

    Adiciona jogo

---
+++ pythonProject/jogo_de_labirinto.py
@@ -0,0 +1,15 @@
+print('Você está em uma floresta escuta, a luz do luar ilimina os itens a sua frente....')
+print('--> Itens: MACHADO, ÁGUA, BARRAS DE CEREAL e LANTERNA')
+print('Há uma trilha ao NORTE, ao SUL e ao LESTE')
+comandos = input('Digite um comando: ')
+if comandos == 'pegar itens':
+    print('Você pegou os itens')
+elif comandos == 'sair':
+    while comandos == 'sair':
+        print('POR QUÊ SAIR??????')
+else :
+    print('REINICIE O JOGO')
+print('Você está em uma floresta escuta, a luz do luar ilimina os itens a sua frente....')
+print('--> Inventário: MACHADO, ÁGUA, BARRAS DE CEREAL e LANTERNA')
+print('Há uma trilha ao NORTE, ao SUL e ao LESTE')
+
